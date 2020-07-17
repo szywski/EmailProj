debbee39eb80789ec54059edcb3666149a505931
Szymon Chmielewski <szywski27@gmail.com>
2020-07-18 01:02:32 +0200

    add req: slf4j, apache commons lang

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module org.openjfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.commons.lang3;
+    requires slf4j.api;
+    requires slf4j.simple;
 
 
     opens org.openjfx to javafx.fxml;
