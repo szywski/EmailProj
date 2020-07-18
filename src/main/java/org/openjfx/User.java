e40a154538e59444366af243aec96196ee89febd
Szymon Chmielewski <szywski27@gmail.com>
2020-07-18 23:54:24 +0200

    update methods: generateRandomPassword, changeUserPassowrd, serPassword,generateEmailAddress

--- src/main/java/org/openjfx/User.java
+++ src/main/java/org/openjfx/User.java
@@ -1,5 +1,7 @@
 package org.openjfx;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class User {
 
     String name;
@@ -19,12 +21,30 @@ public class User {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    void setPassword(String pass) {
+        this.password = pass;
+    }
+    public void changeUserPassword(String password){
+        this.setPassword(password);
     }
 
     @Override
     public String toString() {
         return eMail +" "+getPassword();
+    }
+    public String generateRandomPassword(){
+        String password = RandomStringUtils.randomAlphabetic(8);
+        this.setPassword(password);
+        return password;
+    }
+
+    public StringBuilder generateEmailAddress(){
+        StringBuilder address = new StringBuilder();
+        address.append(name + ".");
+        address.append(lastName + "@");
+        address.append(department);
+        address.append("company.com");
+        eMail = address.toString();
+        return address;
     }
 }
